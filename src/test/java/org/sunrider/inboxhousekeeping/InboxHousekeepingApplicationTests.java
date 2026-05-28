e8ad9445dfcc06e6b081a44e6171d47a8dc65984
a.rabuev <rabueva75@mail.ru>
2026-05-28 12:55:59 +0200

    Initial commit

---
+++ src/test/java/org/sunrider/inboxhousekeeping/InboxHousekeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sunrider.inboxhousekeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxHousekeepingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
