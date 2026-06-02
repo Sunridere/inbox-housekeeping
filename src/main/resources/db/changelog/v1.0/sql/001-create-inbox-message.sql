eb59472c9ba3f81367f8b358bbd9555372a30f10
a.rabuev <rabueva75@mail.ru>
2026-06-02 12:26:12 +0200

    исправлен скрипт создания партиций динамический от текущей даты

--- src/main/resources/db/changelog/v1.0/sql/001-create-inbox-message.sql
+++ src/main/resources/db/changelog/v1.0/sql/001-create-inbox-message.sql
@@ -22,34 +22,6 @@ CREATE TABLE inbox_message
 CREATE TABLE inbox_message_default
     PARTITION OF inbox_message DEFAULT;
 
-CREATE TABLE inbox_message_2026_05_27
-    PARTITION OF inbox_message
-    FOR VALUES FROM ('2026-05-27') TO ('2026-05-28');
-
-CREATE TABLE inbox_message_2026_05_28
-    PARTITION OF inbox_message
-    FOR VALUES FROM ('2026-05-28') TO ('2026-05-29');
-
-CREATE TABLE inbox_message_2026_05_29
-    PARTITION OF inbox_message
-    FOR VALUES FROM ('2026-05-29') TO ('2026-05-30');
-
-CREATE TABLE inbox_message_2026_05_30
-    PARTITION OF inbox_message
-    FOR VALUES FROM ('2026-05-30') TO ('2026-05-31');
-
-CREATE TABLE inbox_message_2026_05_31
-    PARTITION OF inbox_message
-    FOR VALUES FROM ('2026-05-31') TO ('2026-06-01');
-
-CREATE TABLE inbox_message_2026_06_01
-    PARTITION OF inbox_message
-    FOR VALUES FROM ('2026-06-01') TO ('2026-06-02');
-
-CREATE TABLE inbox_message_2026_06_02
-    PARTITION OF inbox_message
-    FOR VALUES FROM ('2026-06-02') TO ('2026-06-03');
-
 -- индексы создаются на родительской таблице
 -- PostgreSQL автоматически создаст их на всех партициях
 CREATE INDEX idx_inbox_message_status
@@ -60,3 +32,61 @@ CREATE INDEX idx_inbox_message_created_at
 
 CREATE INDEX idx_inbox_message_event_id
     ON inbox_message (event_id);
+
+-- Функция: создать партицию за конкретный день
+CREATE OR REPLACE FUNCTION create_inbox_partition(p_date DATE)
+    RETURNS VOID LANGUAGE plpgsql AS $body$
+DECLARE
+    v_partition_name TEXT;
+    v_date_from      DATE;
+    v_date_to        DATE;
+BEGIN
+    v_partition_name := 'inbox_message_' || TO_CHAR(p_date, 'YYYY_MM_DD');
+    v_date_from      := p_date;
+    v_date_to        := p_date + INTERVAL '1 day';
+
+    -- Пропускаем, если партиция уже существует
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_class c
+        JOIN pg_namespace n ON n.oid = c.relnamespace
+        WHERE c.relname = v_partition_name
+          AND n.nspname = current_schema()
+    ) THEN
+        EXECUTE format(
+            'CREATE TABLE %I PARTITION OF inbox_message
+             FOR VALUES FROM (%L) TO (%L)',
+            v_partition_name,
+            v_date_from,
+            v_date_to
+        );
+
+        RAISE NOTICE 'Partition created: %', v_partition_name;
+    ELSE
+        RAISE NOTICE 'Partition already exists: %', v_partition_name;
+    END IF;
+END;
+$body$;
+
+-- Функция: создать партиции за диапазон дат
+CREATE OR REPLACE FUNCTION create_inbox_partitions(
+    p_days_back    INT DEFAULT 7,
+    p_days_forward INT DEFAULT 7
+)
+    RETURNS VOID LANGUAGE plpgsql AS $body$
+DECLARE
+    v_current_date DATE;
+BEGIN
+    v_current_date := CURRENT_DATE - p_days_back;
+
+    WHILE v_current_date <= CURRENT_DATE + p_days_forward LOOP
+            PERFORM create_inbox_partition(v_current_date);
+            v_current_date := v_current_date + INTERVAL '1 day';
+        END LOOP;
+END;
+$body$;
+
+-- Первичная инициализация:
+SELECT create_inbox_partitions(
+               p_days_back    => 7,
+               p_days_forward => 7
+       );
